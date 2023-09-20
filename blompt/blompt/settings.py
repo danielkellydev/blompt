fc2cb5a3e84f604e556757021c91c3c02e31d6d4
danielkellydev <danielkellydev@gmail.com>
2023-09-20 10:39:28 +1000

    updated settings.py static dir

--- blompt/blompt/settings.py
+++ blompt/blompt/settings.py
@@ -128,6 +128,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "blogwriter", "static"),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, "blogwriter", "static")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
