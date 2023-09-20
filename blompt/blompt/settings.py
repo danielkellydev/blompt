3683c108e1905f6e5e1bd215055c9fcbf53d414d
danielkellydev <danielkellydev@gmail.com>
2023-09-20 10:46:56 +1000

    updated settings.py static dir

--- blompt/blompt/settings.py
+++ blompt/blompt/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "blogwriter", "static"),
+    os.path.join("../blogwriter/static",)
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "blogwriter", "static")
